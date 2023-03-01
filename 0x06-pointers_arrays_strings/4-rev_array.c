b4b1b84158f8a99779eacfad730736fca13cb08d
youssef4628 <moyoussef46@gmail.com>
2023-03-01 15:32:11 -0800

    finish

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+ * @n: the lenght of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
