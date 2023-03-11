9b26ee56f1bc8ac43c0a731433cb3bcaed4208f2
youssef4628 <moyoussef46@gmail.com>
2023-03-11 14:07:09 -0800

    final

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,22 @@
-#include "main.h"
+#include <main.h>
 #include <stdio.h>
+#include <string.h>
 
-/**
- * main - this is a function to print its name
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 for success
- */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", *argv);
-	return (0);
+int main(int argc, char *argv[]) {
+    // Get the program name from the first argument
+    char *program_name = argv[0];
+
+    // Find the last occurrence of a directory separator in the program name
+    char *last_sep = strrchr(program_name, '/');
+
+    // If a directory separator was found, increment the pointer to get the filename
+    if (last_sep != NULL) {
+        program_name = last_sep + 1;
+    }
+
+    // Print the program name
+    printf("%s\n", program_name);
+
+    return 0;
 }
+
