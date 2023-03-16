fc3d59b4c4137d8e7e9834086374783dc722fde8
youssef4628 <moyoussef46@gmail.com>
2023-03-16 06:20:32 -0700

    aref

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,22 +1,7 @@
-#include <main.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // Get the program name from the first argument
-    char *program_name = argv[0];
-
-    // Find the last occurrence of a directory separator in the program name
-    char *last_sep = strrchr(program_name, '/');
-
-    // If a directory separator was found, increment the pointer to get the filename
-    if (last_sep != NULL) {
-        program_name = last_sep + 1;
-    }
-
-    // Print the program name
-    printf("%s\n", program_name);
-
-    return 0;
+    printf("%s\n", argv[0]);
+    return EXIT_SUCCESS;
 }
-
